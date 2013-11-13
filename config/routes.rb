15b1f4c56d008dae6f93a7591821578ba8bb95ed
Victor Pontis <vpontis@gmail.com>
2013-11-13 18:29:32 -0500

    added signing in and out for player

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 VpontisStubailoDsessomsSanthoshnarayanFinal::Application.routes.draw do
+  devise_for :players
+
+  root to: redirect('/players/sign_in')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
