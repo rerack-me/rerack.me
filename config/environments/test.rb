68aab417bdfa405243468b2fef96567761f0da4a
Victor Pontis <vpontis@gmail.com>
2013-11-16 14:16:38 -0800

    fixed merge conflict

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,11 @@ VpontisStubailoDsessomsSanthoshnarayanFinal::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.default :charset => "utf-8"
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
