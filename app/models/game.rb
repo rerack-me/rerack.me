a9e0fae9efe31e59804a71efbc8b49fe9b849bfe
Sashko Stubailo <sashko@mit.edu>
2013-11-17 06:00:44 -0500

    Fixed up game validation
    Made messages more readable
    Added error about usernames not found

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,6 +18,7 @@ class Game < ActiveRecord::Base
   end
 
   def winner_usernames=(usernames)
+    @winner_usernames = usernames
     self.winners = Player.where(:username => usernames)
   end
   
@@ -26,13 +27,31 @@ class Game < ActiveRecord::Base
   end
   
   def loser_usernames=(usernames)
+    @loser_usernames = usernames
     self.losers = Player.where(:username => usernames)
   end
 
   # validation
-  validates :winners, length: {is: 2}
-  validates :losers, length: {is: 2}
+  validate :all_users_found
   validate :users_are_unique
+ 
+  validates :winners, length: {is: 2, message: "should have 2 players."}
+  validates :losers, length: {is: 2, message: "should have 2 players."}
+
+  def all_users_found
+    if @winner_usernames
+      winner_not_found = @winner_usernames - winner_usernames
+    end
+    if @loser_usernames
+      loser_not_found = @loser_usernames - loser_usernames
+    end
+
+    total_not_found = winner_not_found + loser_not_found
+
+    if total_not_found.any?
+      errors.add(:players, total_not_found.join(", ") + " not found.")
+    end
+  end
 
   def users_are_unique
     temp_players = winners + losers
