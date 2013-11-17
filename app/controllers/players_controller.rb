f2ffa62f002ef1c5d84ee8da9db08cfb46f4382d
Victor Pontis <vpontis@gmail.com>
2013-11-16 20:15:24 -0800

    added players ranking page and elo calculation

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,10 +7,14 @@ class PlayersController < ApplicationController
   end
 
   def index
-    @players = Player.all
+    calculate_rankings
+    @players = Player.all.order('points DESC')
     respond_to do |format|
       format.json {
         render json: @players.map { |player| player.username }
+      }
+      format.html {
+        render 'index'
       }
     end
   end
@@ -19,5 +23,35 @@ class PlayersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_player
       @player = Player.find(params[:id])
+    end
+
+    def calculate_rankings
+      Player.all.each { |player| player.points = 1000; player.save }
+
+      Game.all.order('created_at asc').each do |game|
+        winners = game.winners
+        losers = game.losers
+
+        winners_rating = (winners[0].points + winners[1].points)/2
+        losers_rating = (losers[0].points + losers[1].points)/2
+
+        q_winners = 10**(winners_rating/400)
+        q_losers = 10**(losers_rating/400)
+
+        expected_winners = q_winners/(q_winners + q_losers)
+        expected_losers = q_losers/(q_winners + q_losers)
+
+        point_change = 32*(1 - expected_winners)
+
+        winners.each do |winner|
+          winner.points += point_change
+          winner.save
+        end
+
+        losers.each do |loser|
+          loser.points -= point_change
+          loser.save
+        end
+      end
     end
 end
