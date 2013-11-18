de31b3848410021f8ade189a7cc9b0ec2aed3fda
Victor Pontis <vpontis@gmail.com>
2013-11-17 18:39:44 -0800

    added mailer configuration for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 VpontisStubailoDsessomsSanthoshnarayanFinal::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'rerackme.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  } 
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
