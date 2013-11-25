5e8b8e72e7195a2e383cd0a473addabe032ebadf
Victor Pontis <vpontis@gmail.com>
2013-11-25 01:08:49 -0500

    added groups index update and show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_filter :set_group, only: []
+  before_filter :set_group, only: [:show, :update, :edit]
 
   def index
     @groups = Group.all
@@ -11,11 +11,38 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to group_path(@group) }
+        format.json { render action: 'show', status: :created, location: @group }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    username = params[:group][:username]
+
+    respond_to do |format|
+      if @group.add_player_by_username(username)
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'show' }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
     def group_params
-      params.require(:group).permit(player_usernames: [])
+      params.require(:group).permit(:name, :username, player_usernames: [])
     end
 
     def set_group
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,16 +1,26 @@
 class Group < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   # players
   has_many :group_players
   has_many :players, through: :group_players, source: 'player'
 
   def player_usernames=(usernames)
-    @player_usernames = usernames
     self.players = Player.where(username: usernames)
   end
 
   def player_usernames
     self.players.map {|p| p.username}
+  end
+
+  def add_player_by_username(username)
+    player = Player.find_by(username: username)
+
+    if player.nil? 
+      errors.add(:players, "#{username} not found.")
+      false
+    else
+      self.players << player
+    end
   end
 end
