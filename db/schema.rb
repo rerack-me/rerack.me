dbe27b4d1aaae97171fccb744df2cdf6cb84d3ff
Santhosh Narayan <santhoshnarayan@mit.edu>
2013-12-01 17:51:38 -0500

    player test doesn't pass, doesnt look like the method is working?

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20131125181341) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "games_count"
     t.float    "points",                 default: 1000.0
   end
 
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -24,16 +24,15 @@ class GameTest < ActiveSupport::TestCase
     assert g.losers.count == 2, 'The losers didnt save!'
   end
 
-  test "players"
+  test "players" do
     g = Game.new
     g.winners = [players(:a), players(:b)]
     g.losers = [players(:c), players(:d)]
     g.save
-    assert_equal g.players.count, 4, "Should have returned that there are 4 players in a game"
-
+    assert g.players.map {|p| p.username} == [players(:a), players(:b), players(:c), players(:d)], g.players.map {|p| p.username}
   end
 
-  test "winner_usernames"
+  test "winner_usernames" do
     g = Game.new
     g.winners = [players(:a), players(:b)]
     g.losers = [players(:c), players(:d)]
@@ -42,7 +41,7 @@ class GameTest < ActiveSupport::TestCase
     assert g.winner_usernames == ["alice", "bob"], "should return winner usernames to be alice and bob"
   end
 
-  test "loser usernames"
+  test "loser usernames" do
     g = Game.new
     g.winners = [players(:a), players(:b)]
     g.losers = [players(:c), players(:d)]
@@ -51,7 +50,7 @@ class GameTest < ActiveSupport::TestCase
     assert g.loser_usernames == ["calvin", "david"], "should return loser usernames to be calvin and david"
   end
 
-  test "confirmed?"
+  test "confirmed?" do
 
     g = Game.new
     g.winners = [players(:a), players(:b)]
@@ -62,7 +61,7 @@ class GameTest < ActiveSupport::TestCase
     assert_equal g.confirmed?, true, "game was not confirmed, should have been"
   end
 
-  test "confirm"
+  test "confirm" do
 
     g = Game.new
     g.winners = [players(:a), players(:b)]
@@ -73,13 +72,21 @@ class GameTest < ActiveSupport::TestCase
     assert_equal g.confirmed?, true, "game was not confirmed, should have been"
   end
 
-  test "all users found"
-  end
+  # test "all users found" do
+  # end
 
-  test "users are unique"
-  end
+  # test "users are unique" do
+  # end
 
-  test "transfer points"
-end
+  test "transfer points" do
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.save
+    assert_equal players(:a).points, 1016, "player a points"
+    assert_equal players(:b).points, 1016, "player b points"
+    assert_equal players(:c).points, 984, "player c points"
+    assert_equal players(:d).points, 984, "player d points"
+  end
 
 end
