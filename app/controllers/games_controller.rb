6fa9c78092d8b8c58220b8271b04f7b2981f1a10
David <dvd_sssms@yahoo.com>
2013-12-01 16:24:21 -0500

    renamed sorted games and rearranged the view. still looks bad though

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,9 +5,9 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games_24_old = Game.where("created_at >= ? AND confirmed == ?", Time.now-24.hours, true).order('created_at DESC')
-    @games_week_old = Game.where("created_at >= ? AND created_at < ? AND confirmed == ?", 1.week.ago, Time.now-24.hours, true).order('created_at DESC')
-    @games_oldest = Game.where("created_at < ? AND confirmed == ?", 1.week.ago, true).order('created_at DESC')
+    @games_today = Game.where("created_at >= ? AND confirmed == ?", Time.now-24.hours, true).order('created_at DESC')
+    @games_this_week = Game.where("created_at >= ? AND created_at < ? AND confirmed == ?", 1.week.ago, Time.now-24.hours, true).order('created_at DESC')
+    @games_before_this_week = Game.where("created_at < ? AND confirmed == ?", 1.week.ago, true).order('created_at DESC')
   end
 
   # GET /games/1
