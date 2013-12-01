b1510032d3bbf1949dd1b414b881a1c0baecc837
Santhosh Narayan <santhoshnarayan@mit.edu>
2013-12-01 01:51:41 -0500

    added game tests to updated new branch without merge errors

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -23,4 +23,63 @@ class GameTest < ActiveSupport::TestCase
     assert g.winners.count == 2, 'The winners didnt save!'
     assert g.losers.count == 2, 'The losers didnt save!'
   end
+
+  test "players"
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.save
+    assert_equal g.players.count, 4, "Should have returned that there are 4 players in a game"
+
+  end
+
+  test "winner_usernames"
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.save
+
+    assert g.winner_usernames == ["alice", "bob"], "should return winner usernames to be alice and bob"
+  end
+
+  test "loser usernames"
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.save
+
+    assert g.loser_usernames == ["calvin", "david"], "should return loser usernames to be calvin and david"
+  end
+
+  test "confirmed?"
+
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.confirmed = true
+    g.save
+
+    assert_equal g.confirmed?, true, "game was not confirmed, should have been"
+  end
+
+  test "confirm"
+
+    g = Game.new
+    g.winners = [players(:a), players(:b)]
+    g.losers = [players(:c), players(:d)]
+    g.save
+    g.confirm
+
+    assert_equal g.confirmed?, true, "game was not confirmed, should have been"
+  end
+
+  test "all users found"
+  end
+
+  test "users are unique"
+  end
+
+  test "transfer points"
+end
+
 end
