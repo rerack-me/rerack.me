0b6f8a8e3def40743adaa2cfd270fd5b0487cdb2
Victor Pontis <vpontis@gmail.com>
2013-12-01 20:58:07 -0500

    updated schema to have parameterized username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127194831) do
+ActiveRecord::Schema.define(version: 20131201205417) do
 
   create_table "game_losers", force: true do |t|
     t.integer  "player_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20131127194831) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "points",                 default: 1000.0
+    t.string   "parameterized_username"
   end
 
   add_index "players", ["email"], name: "index_players_on_email", unique: true
