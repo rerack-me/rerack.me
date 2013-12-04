c9e7d34504110d5e09782e32555dd9f2d3035b63
Victor Pontis <vpontis@gmail.com>
2013-12-04 13:22:50 -0500

    added logic for goto in rankings

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,12 +6,33 @@ class PlayersController < ApplicationController
   def show
   end
 
+  # GET /players
   require 'will_paginate/array'
   def index
     # calculate_rankings
     @players = Player.order('points DESC')
     @players = @players.reject {|p| p.games_count < 2}
-    @players = @players.paginate(page: params[:page], per_page: 25)
+
+
+    # set the page
+    # page overwrites goto, if specified
+    # if we want to goto a user, choose their page
+    if params[:page].blank? and params[:goto].present?
+      player = Player.find_by_username(params[:goto])
+      # goto player not found or not included in rankings
+      if player.blank? or !@players.include? player
+        flash[:notice] = "Could not find #{params[:goto]} in the rankings."
+        page = params[:page]
+      else
+        # add 1 because pages aren't 0 indexed
+        index = @players.find_index(player)
+        page = index/Player.per_page + 1
+      end  
+    else
+      page = params[:page]
+    end  
+
+    @players = @players.paginate(page: page, per_page: Player.per_page)
   end
 
   private
