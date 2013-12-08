4cf32855eb6441ef00d2eb09a1a1c49ae708e1d0
Victor Pontis <vpontis@gmail.com>
2013-12-08 12:29:20 -0500

    fixed players json inde

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,30 +10,7 @@ class PlayersController < ApplicationController
   # GET /players
   require 'will_paginate/array'
   def index
-    # calculate_rankings
-    @players = Player.order('points DESC')
-    @players = @players.select {|p| p.is_ranked?}
-    @group = Group.global
-
-    # set the page
-    # page overwrites goto, if specified
-    # if we want to goto a user, choose their page
-    if params[:page].blank? and params[:goto].present?
-      player = Player.find_by_username(params[:goto])
-      # goto player not found or not included in rankings
-      if player.blank? or !@players.include? player
-        flash[:notice] = "Could not find #{params[:goto]} in the rankings."
-        page = params[:page]
-      else
-        # add 1 because pages aren't 0 indexed
-        index = @players.find_index(player)
-        page = index/Player.per_page + 1
-      end  
-    else
-      page = params[:page]
-    end  
-
-    @players = @players.paginate(page: page, per_page: Player.per_page)
+    @players = Player.all
   end
 
   private
